bf958fad8f9992046985488bef24fbcfe95f1d1a
tursunbaevz <zuma9405@gmail.com>
2016-09-13 17:45:38 +0300

    commit with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+user1 = User.create(name: 'admin', email: 'admin@gmail.com', password: 'password', password_confirmation: 'password')
 
 
- 
+
+
+
+post1 = Post.create(
+	description: 'new post', 
+	image:  File.new("#{Rails.root}/public/uploads/1.jpg"),
+	user_id: user1.id
+
+
+	)
+
+ Comment.create(
+ 	content: 'bla bla', 
+ 	post_id: post1.id,
+ 	user_id: user1.id
+ 	)
